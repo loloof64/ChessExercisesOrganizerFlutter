5dd505abe637deed46a8e1a79dc3badc0086c99b
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-02-12 11:05:03 +0100

    Set up a chess board on the game page.

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chess_board/flutter_chess_board.dart';
+import 'package:go_router/go_router.dart';
 
 class GameScreen extends StatefulWidget {
   const GameScreen({Key? key}) : super(key: key);
@@ -8,13 +10,26 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  ChessBoardController _controller = ChessBoardController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Game'),
+        title: const Text('Game'),
       ),
-      body: SizedBox(),
+      body: Center(
+          child: Column(
+        children: [
+          ChessBoard(controller: _controller),
+          ElevatedButton(
+            onPressed: () {
+              GoRouter.of(context).go('/');
+            },
+            child: const Text('Go back to home'),
+          )
+        ],
+      )),
     );
   }
 }
