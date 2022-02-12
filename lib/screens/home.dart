881f641c882dc3175bcf2d893d09b51c29a27a3b
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-02-12 21:09:03 +0100

    Changed application icon and added license.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,21 @@
+/*
+    Chess exercises organizer : oad your chess exercises and train yourself against the device.
+    Copyright (C) 2022  Laurent Bernabe <laurent.bernabe@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
