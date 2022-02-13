741349eb412c260acb387186e4fafcd1ee1077af
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-02-13 22:39:10 +0100

    Added coordinates to RichChessboard

--- lib/components/richboard.dart
+++ lib/components/richboard.dart
@@ -24,12 +24,91 @@ class RichChessboard extends StatefulWidget {
 class _RichChessboardState extends State<RichChessboard> {
   @override
   Widget build(BuildContext context) {
-    return Chessboard(
-      fen: widget.fen,
-      size: widget.size,
-      onMove: widget.onMove,
-      onPromote: widget.onPromote,
-      orientation: widget.orientation,
+    final commonTextStyle = TextStyle(
+      color: Colors.yellow.shade400,
+      fontWeight: FontWeight.bold,
+      fontSize: widget.size * 0.04,
+    );
+    final topFilesCoordinates = [0, 1, 2, 3, 4, 5, 6, 7].map((file) {
+      final letterOffset =
+          widget.orientation == BoardColor.WHITE ? file : 7 - file;
+      final letter = String.fromCharCode('A'.codeUnitAt(0) + letterOffset);
+      return Positioned(
+        top: widget.size * 0.005,
+        left: widget.size * (0.09 + 0.113 * file),
+        child: Text(
+          letter,
+          style: commonTextStyle,
+        ),
+      );
+    });
+
+    final bottomFilesCoordinates = [0, 1, 2, 3, 4, 5, 6, 7].map((file) {
+      final letterOffset =
+          widget.orientation == BoardColor.WHITE ? file : 7 - file;
+      final letter = String.fromCharCode('A'.codeUnitAt(0) + letterOffset);
+      return Positioned(
+        bottom: widget.size * 0.003,
+        left: widget.size * (0.09 + 0.113 * file),
+        child: Text(
+          letter,
+          style: commonTextStyle,
+        ),
+      );
+    });
+
+    final leftRanksCoordinates = [0, 1, 2, 3, 4, 5, 6, 7].map((rank) {
+      final letterOffset =
+          widget.orientation == BoardColor.WHITE ? 7 - rank : rank;
+      final letter = String.fromCharCode('1'.codeUnitAt(0) + letterOffset);
+      return Positioned(
+        left: widget.size * 0.012,
+        top: widget.size * (0.09 + 0.113 * rank),
+        child: Text(
+          letter,
+          style: commonTextStyle,
+        ),
+      );
+    });
+
+    final rightRanksCoordinates = [0, 1, 2, 3, 4, 5, 6, 7].map((rank) {
+      final letterOffset =
+          widget.orientation == BoardColor.WHITE ? 7 - rank : rank;
+      final letter = String.fromCharCode('1'.codeUnitAt(0) + letterOffset);
+      return Positioned(
+        right: widget.size * 0.012,
+        top: widget.size * (0.09 + 0.113 * rank),
+        child: Text(
+          letter,
+          style: commonTextStyle,
+        ),
+      );
+    });
+
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          color: Colors.indigo.shade300,
+          width: widget.size,
+          height: widget.size,
+          child: Stack(
+            children: [
+              ...topFilesCoordinates,
+              ...bottomFilesCoordinates,
+              ...leftRanksCoordinates,
+              ...rightRanksCoordinates,
+            ],
+          ),
+        ),
+        Chessboard(
+          fen: widget.fen,
+          size: widget.size * 0.9,
+          onMove: widget.onMove,
+          onPromote: widget.onPromote,
+          orientation: widget.orientation,
+        ),
+      ],
     );
   }
 }
